a05312016fd12025bbaa6419944ca76617b04a57
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-26 23:26:38 -0300

    add new pages

---
+++ src/pages/Book.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default function Book() {
+  return <View>
+    <Text>Hello</Text>
+  </View>
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+import logo from '../assets/logo.png'
+
+export default function Login() {
+  return <View>
+    <Image source={logo}/>
+  </View>
+}
