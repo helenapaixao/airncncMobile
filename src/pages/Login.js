ef2c94526cd2b42706b432da9c089d403d53523c
Helena Paixão <hp.helenapaixao@gmail.com>
2019-10-27 00:49:14 -0300

    config api

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import {View, Image, StyleSheet,Text, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {View, Image, StyleSheet,Text, TextInput, TouchableOpacity} from 'react-native';
+import api from '../services/api'
 import logo from '../assets/logo.png'
 
 export default function Login() {
+  const [email,setEmail] = useState('');
+  const [techs,setTechs] = useState('');
+  async function handleSubmit(){
+    console.log(email);
+    console.log(techs);
+
+  }
   return <View style={styles.container}>
     <Image source={logo}/>
+
     <View style={styles.form}>
     <Text style={styles.label}>SEU E-MAIL *</Text>
     <TextInput
@@ -14,9 +23,22 @@ export default function Login() {
     keyboardType="email-address"
     autoCapitalize="none"
     autoCorrect={false}
-    >
-
-    </TextInput>
+    value={email}
+    onChangeText={setEmail}
+    />
+    <Text style={styles.label}>TECNOLOGIAS *</Text>
+    <TextInput
+    style={styles.input}
+    placeholder="Tecnologias de interesse"
+    placeholderTextColor="#999"
+    autoCapitalize="words"
+    autoCorrect={false}
+    onChangeText={setTechs}
+    />
+    <TouchableOpacity onPress={handleSubmit} style={styles.button}>
+      <Text style={styles.buttonText}>Encontrar spots</Text>
+    </TouchableOpacity>
+   
     </View>
   </View>
 }
@@ -46,5 +68,17 @@ const styles = StyleSheet.create({
     height: 44,
     marginBottom:20,
     borderRadius:2
+  },
+  button: {
+    height: 42,
+    backgroundColor:'#f05a5b',
+    justifyContent:'center',
+    alignItems:'center',
+    borderRadius: 2,
+  },
+  buttonText:{
+    color:'#fff',
+    fontWeight:'bold',
+    fontSize: 16,
   },
 })
